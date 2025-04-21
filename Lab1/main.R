43a6741a685d185ff2239098dcb8e1c1e2e3cee0
Floressek <szymon.florek@student.wat.edu.pl>
2025-04-21 19:54:39 +0200

    Refactor and enhance data preprocessing steps.
    Improved readability and modularity by restructuring the data import section and adding detailed missing value analysis. Preserved original variables for visualization, streamlined variable selection, and ensured cleaner data preprocessing for predictive modeling.

--- Lab1/main.R
+++ Lab1/main.R
@@ -1,30 +1,45 @@
 library(tidyverse)
 
-# Zadanie 1
-wesbrook_dataset <- read_csv("http://jolej.linuxpl.info/Wesbrook.csv",
-                             col_types = "ificifffffffffffffinnnnniifnnnn")
+
+# Zadanie 1: Import danych
+url <- "http://jolej.linuxpl.info/Wesbrook.csv"
+wesbrook_dataset <- read_csv(url, col_types = "ificifffffffffffffinnnnniifnnnn")
 
 glimpse(wesbrook_dataset)
 
+# Konwersja daty
 wesbrook_dataset <- wesbrook_dataset %>%
   mutate(INDUPDT = mdy(INDUPDT))
 
 glimpse(wesbrook_dataset)
 
-# Zadanie 2 - determine and eliminate unnecessary columns
-summary(wesbrook_dataset)
+# Zadanie 2: Wstępna analiza zbioru
+cat("\n Liczba i procent brakujących wartości według kolumn:\n")
+missing_data <- data.frame(
+  Variable = names(wesbrook_dataset),
+  Count = colSums(is.na(wesbrook_dataset)),
+  Percentage = round(colSums(is.na(wesbrook_dataset)) / nrow(wesbrook_dataset) * 100, 2)
+)
+print(missing_data)
 
-# Zadanie 3
-# Checking for missing values
+# Zachowanie oryginalnej zmiennej WESBROOK do wizualizacji
+wesbrook_dataset$WESBROOK_original <- wesbrook_dataset$WESBROOK
+
+# Zadanie 3: Analiza istotności zmiennych
 wesbrook_dataset <- subset(
   wesbrook_dataset,
-  !is.na("MOV_DWEL") &
-    !is.na("HH_1PER") &
-    !is.na("HH_2PER"))
+  !is.na(MOV_DWEL) &
+    !is.na(HH_1PER) &
+    !is.na(HH_2PER)
+)
 
-# Deleting columns we deemed unnecessary in zadanie 2
+# Usuwanie kolumn ktore uznaliśmy za zbędne w kontekście predykcji
 wesbrook_dataset <- subset(wesbrook_dataset,
                            select = -c(ID, GRADYR1,
                                        FACULTY1, DEPT1,
-                                       MAJOR1, FRSTYEAR, BIGBLOCK, EA, INDUPDT))
+                                       MAJOR1, FRSTYEAR,
+                                       BIGBLOCK, EA,
+                                       INDUPDT))
+
+cat("\n Statystyki po wstępnym czyszczeniu danych:\n")
 summary(wesbrook_dataset)
