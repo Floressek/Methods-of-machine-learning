e3d2b3f06275798a1caf61e3910585c93661f68c
Floressek <szymon.florek@student.wat.edu.pl>
2025-04-29 21:21:57 +0200

    Enhance data preparation in main.R by adding dummy variable creation and splitting datasets into training and testing sets.

--- Lab2/main.R
+++ Lab2/main.R
@@ -1,3 +1,4 @@
+# Wymagane biblioteki
 library(tidyverse)
 library(caret)
 library(class)
@@ -13,7 +14,8 @@ wesbrook_dataset <- read_csv("http://jolej.linuxpl.info/Wesbrook.csv",
                              col_types = "ificifffffffffffffinnnnniifnnnn")
 
 # Ta sekcja jest identyczna do obrobki danych z poprzednich laboratoriów 1-2,
-# pomijam tu kroki eliminacji zmiennych z duzą liczbą NA values oraz wykrywania cech znaczacych. Zostało to wykonane w poprzednich laboratoriach które także załączę w folderze w razie wyjasnienia wybrania danej cechy.
+# pomijam tu kroki eliminacji zmiennych z duzą liczbą NA values oraz wykrywania cech znaczacych. Zostało to wykonane w poprzednich laboratoriach, które także załączę w folderze w razie wyjasnienia wybrania danej cechy.
+
 # Konwersja daty
 wesbrook_dataset <- wesbrook_dataset %>%
   mutate(INDUPDT = mdy(INDUPDT))
@@ -73,5 +75,43 @@ cat("\n Statystyki po normalizacji:\n")
 summary(wesbrook2)
 
 # Stworzenie zbiorów treningowych i testowych
+# Zbiory z podzielonymi klasami oraz cechami
 
-# Zbiór z podzielonymi klasami oraz cechami
+# Zbiór z klasami
+wesbrook_labels <- wesbrook2 %>% select(WESBROOK)
+# Zbiór z cechami, traktujemy go jako data.frame
+wesbrook_features <- wesbrook2 %>% select(-WESBROOK)
+wesbrook_features <- as.data.frame(wesbrook_features)
+
+# Tworzymy formułę zawierającą wszystkie zmienne kategoryczne
+categorical_vars <- c("PARENT", "CHILD", "SPOUSE", "SEX", "FACSTAFF",
+                      "ATHLTCS", "OTHERACT", "PROV")
+# ~ 0 + oznacza ze moduł nie ma interceptu, a zmienne kategoryczne są traktowane jako zmienne wskaźnikowe (dummy variables) czyli np jak SEX ma F i M to otrzymamt 2 kolumny SEXM oraz SEXF
+formula_text <- paste("~ 0 +", paste(categorical_vars, collapse = " + "))
+formula_obj <- as.formula(formula_text)
+
+# Pakiet dummies nie dzialał wiec zastosujemy model.matrix jako alternatywę
+dummy_matrix <- model.matrix(formula_obj, data = wesbrook_features)
+# Idea tej macierzy jest taka, że dla każdej zmiennej kategorycznej tworzy kolumny z wartościami 0 i 1, które wskazują, czy dany wiersz należy do danej kategorii.
+
+# Łączymy macierz dummy z oryginalnym zbiorem cech
+wesbrook_features_numeric <- wesbrook_features %>%
+  select(TOTLGIVE, AVE_INC, DWEL_VAL)
+wesbrook_features <- cbind(wesbrook_features_numeric, as.data.frame(dummy_matrix))
+
+# Zeby zapewnic deterministyczność wyników, ustawiamy ziarno
+set.seed(1234) #FIXME: test different seeds for data splitting
+# Ustalamy proporcje podziału
+sample_index <- sample(nrow(wesbrook_features), round(nrow(wesbrook_features) * 0.75), replace = FALSE)
+
+# Tworzymy zbiory treningowe i testowe
+wesbrook_features_train <- wesbrook_features[sample_index, ]
+wesbrook_features_test <- wesbrook_features[-sample_index, ]
+
+# Zbiory z klasami
+wesbrook_labels_train <- as.factor(wesbrook_labels[sample_index, ]$WESBROOK)
+wesbrook_labels_test <- as.factor(wesbrook_labels[-sample_index, ]$WESBROOK)
+
+# Zliczemy liczby klas w zbiorach
+table(wesbrook_labels_train)
+table(wesbrook_labels_test)
